f0ffc032eeb61ab2f0ee3d6adffbb8e6bc67f294
Stellaerranti <sir.ushakov-am@yandex.ru>
2020-11-18 09:31:10 +0300

    File manager is added

--- N7/N7/MyForm.h
+++ N7/N7/MyForm.h
@@ -1,5 +1,13 @@
 #pragma once
-
+#include <iostream>              
+#include <string>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <iomanip>
+#include <direct.h>
 namespace N7 {
 
 	using namespace System;
@@ -8,7 +16,8 @@ namespace N7 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-
+	using namespace std;
+	
 	/// <summary>
 	/// Сводка для MyForm
 	/// </summary>
@@ -34,6 +43,9 @@ namespace N7 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
+	private: System::Windows::Forms::Button^  button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +60,46 @@ namespace N7 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 260);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+		
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e)
+	{
+		openFileDialog1->ShowDialog();
+		System::String^ path_f = openFileDialog1->FileName;
+		string vsSt = "";
+		for (int i = 0; i < path_f->Length; i++)
+			vsSt += (char)path_f[i];
+		const char * stt = vsSt.c_str();
+
+	}
 	};
 }
