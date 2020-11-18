b45f78d7714d1b30fdcba807db16dd07ad4895b8
Stellaerranti <sir.ushakov-am@yandex.ru>
2020-11-18 11:13:33 +0300

    Charts added. Multilanguage is applicable.

--- N7/N7/MyForm.h
+++ N7/N7/MyForm.h
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <iomanip>
 #include <direct.h>
+#include <clocale> 
 namespace N7 {
 
 	using namespace System;
@@ -17,6 +18,7 @@ namespace N7 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 	using namespace std;
+	using namespace Runtime::InteropServices;
 	
 	/// <summary>
 	/// Сводка для MyForm
@@ -45,6 +47,14 @@ namespace N7 {
 		}
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  path_box;
+
+
+
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  z1;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart2;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart3;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
 	protected:
 
 	private:
@@ -60,8 +70,26 @@ namespace N7 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea3 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->path_box = (gcnew System::Windows::Forms::TextBox());
+			this->z1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->chart3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->z1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart3))->BeginInit();
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// openFileDialog1
@@ -70,36 +98,135 @@ namespace N7 {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->Location = System::Drawing::Point(6, 47);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(63, 28);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"File";
+			this->button1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->button1->TextImageRelation = System::Windows::Forms::TextImageRelation::TextAboveImage;
+			this->button1->UseMnemonic = false;
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// path_box
+			// 
+			this->path_box->Location = System::Drawing::Point(6, 19);
+			this->path_box->Name = L"path_box";
+			this->path_box->Size = System::Drawing::Size(504, 20);
+			this->path_box->TabIndex = 1;
+			// 
+			// z1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->z1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->z1->Legends->Add(legend1);
+			this->z1->Location = System::Drawing::Point(11, 127);
+			this->z1->Name = L"z1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series1->Legend = L"Legend1";
+			series1->Name = L"Series1";
+			this->z1->Series->Add(series1);
+			this->z1->Size = System::Drawing::Size(300, 300);
+			this->z1->TabIndex = 4;
+			this->z1->Text = L"chart1";
+			// 
+			// chart2
+			// 
+			chartArea2->Name = L"ChartArea1";
+			this->chart2->ChartAreas->Add(chartArea2);
+			legend2->Name = L"Legend1";
+			this->chart2->Legends->Add(legend2);
+			this->chart2->Location = System::Drawing::Point(317, 127);
+			this->chart2->Name = L"chart2";
+			series2->ChartArea = L"ChartArea1";
+			series2->Legend = L"Legend1";
+			series2->Name = L"Series1";
+			this->chart2->Series->Add(series2);
+			this->chart2->Size = System::Drawing::Size(300, 300);
+			this->chart2->TabIndex = 5;
+			this->chart2->Text = L"chart2";
+			// 
+			// chart3
+			// 
+			chartArea3->Name = L"ChartArea1";
+			this->chart3->ChartAreas->Add(chartArea3);
+			legend3->Name = L"Legend1";
+			this->chart3->Legends->Add(legend3);
+			this->chart3->Location = System::Drawing::Point(623, 127);
+			this->chart3->Name = L"chart3";
+			series3->ChartArea = L"ChartArea1";
+			series3->Legend = L"Legend1";
+			series3->Name = L"Series1";
+			this->chart3->Series->Add(series3);
+			this->chart3->Size = System::Drawing::Size(300, 300);
+			this->chart3->TabIndex = 6;
+			this->chart3->Text = L"chart3";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->groupBox1->Controls->Add(this->path_box);
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Location = System::Drawing::Point(7, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->groupBox1->Size = System::Drawing::Size(518, 85);
+			this->groupBox1->TabIndex = 7;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"File path";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 260);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(1008, 497);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->chart3);
+			this->Controls->Add(this->chart2);
+			this->Controls->Add(this->z1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->z1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart3))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 		
+	void MarshalString(String ^ s, string& os) 
+	{	
+	const char* chars = (const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+	}
+
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e)
 	{
+		setlocale(LC_ALL, "Russian");
 		openFileDialog1->ShowDialog();
 		System::String^ path_f = openFileDialog1->FileName;
-		string vsSt = "";
-		for (int i = 0; i < path_f->Length; i++)
-			vsSt += (char)path_f[i];
-		const char * stt = vsSt.c_str();
+		string s2 = "";
+		MarshalString(path_f, s2);
+		ifstream fin(s2);
+		if (!fin.is_open()) {
+			MessageBox::Show("Unable to open file");
+		}
+		else 
+		{
+			path_box->Text = path_f;
 
+
+		}
 	}
-	};
+
+	
+
+};
 }
